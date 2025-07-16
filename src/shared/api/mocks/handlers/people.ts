be1b91a8000d49791daf63bd82ff4b78d4520313
Anton Nesterov <antonn818@gmail.com>
2025-07-16 14:45:37 +0500

    chore: add people grud

--- src/shared/api/mocks/handlers/people.ts
+++ src/shared/api/mocks/handlers/people.ts
@@ -3,9 +3,8 @@ import { http } from "../http";
 import { faker } from "@faker-js/faker";
 import type { ApiSchema } from "../../schema";
 
-// Mock data storage
-const people: ApiSchema["Person"][] = Array.from({ length: 5 }, (_, i) => ({
-  id: i + 1,
+const people: ApiSchema["Person"][] = Array.from({ length: 5 }, () => ({
+  id: crypto.randomUUID(),
   name: faker.person.fullName(),
   email: faker.internet.email(),
   createdAt: faker.date.past().toISOString(),
@@ -14,5 +13,32 @@ const people: ApiSchema["Person"][] = Array.from({ length: 5 }, (_, i) => ({
 export const peopleHandlers = [
   http.get("/people", () => {
     return HttpResponse.json(people);
+  }),
+
+  http.post("/people", async (ctx) => {
+    const data = await ctx.request.json();
+    const person: ApiSchema["Person"] = {
+      id: crypto.randomUUID(),
+      name: data.name,
+      email: data.email,
+    };
+
+    people.push(person);
+    return HttpResponse.json(person);
+  }),
+
+  http.delete("/people/{personId}", ({ params }) => {
+    const { personId } = params;
+    const index = people.findIndex((person) => person.id === personId);
+    if (index === -1) {
+      return HttpResponse.json(
+        { error: "Person not found", resource: "Person" },
+        { status: 404 },
+      );
+    }
+
+    people.splice(index, 1);
+
+    return HttpResponse.json({});
   }),
 ];
